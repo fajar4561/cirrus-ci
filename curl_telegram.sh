118afc760c2afdb976fb3655176ad4caf16f9324
WalkingDead <lexaxx14@gmail.com>
2021-11-05 13:13:28 +0300

    Update curl_telegram.sh

--- curl_telegram.sh
+++ curl_telegram.sh
@@ -1,7 +1,7 @@
-telegram_id=@WalkingCI
-bot_api=2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q
-curl -s "https://api.telegram.org/bot${bot_api}/sendmessage" -d "text=Привет, сборка LineageOS 18.1 для Redmi 9T / Poco M3 началась!
+telegram_id=
+bot_api=
+curl -s "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendmessage" -d "text=Привет, сборка LineageOS 18.1 для Redmi 9T / Poco M3 началась!
       
       https://cirrus-ci.com/build/$CIRRUS_BUILD_ID
       
-      $show" -d "chat_id=${telegram_id}" -d "parse_mode=HTML"
+      $show" -d "chat_id=@WalkingCI" -d "parse_mode=HTML"
