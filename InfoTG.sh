28391a663d1c29dfc63f01e39e4aa8a7a4fc33e4
WalkingDead <lexaxx14@gmail.com>
2021-11-09 14:43:00 +0300

    cirrus: InfoTG edit [skip ci]

--- InfoTG.sh
+++ InfoTG.sh
@@ -1,5 +1,1 @@
-curl -s "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendmessage" -d "text=Привет, сборка Evolution X (non-OSS) для Redmi 9T / Poco M3 началась!
-      
-      https://cirrus-ci.com/build/$CIRRUS_BUILD_ID
-      
-      $show" -d "chat_id=@WalkingCI" -d "parse_mode=HTML"
+curl -s "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendmessage" -d "text=Привет, сборка Evolution X (non-OSS) для Redmi 9T / Poco M3 началась! https://cirrus-ci.com/build/$CIRRUS_BUILD_ID $show" -d "chat_id=@WalkingCI" -d "parse_mode=HTML"
