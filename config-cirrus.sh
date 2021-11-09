6e6f42d5ba59c7ada6de522acfb46b91406c4b59
WalkingDead <lexaxx14@gmail.com>
2021-11-09 18:37:29 +0300

    cirrus: rebrand config-cirrus.sh [skip ci]

--- config-cirrus.sh
+++ config-cirrus.sh
@@ -1,12 +1,29 @@
-echo "repo init/sync/ccache"
+# ccache download
+echo "ccache downloader"
+df -h
+free -h
+nproc
+cat /etc/os*
+env
+url=https://roms.apon77.workers.dev/ccache/ci3/ccache.tar.gz
+cd /tmp
+time aria2c $url -x16 -s50
+time tar xf ccache.tar.gz
+rm -rf ccache.tar.gz
+
+# Repo
+echo "Repo Init/Sync"
 mkdir -p /tmp/rom
 cd /tmp/rom
 repo init -q --no-repo-verify --depth=1 -u https://github.com/Evolution-X/manifest -b elle -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/WalkingProjekt-juice/manifest.git --depth 1 -b EvoX .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# ccache settings
+echo "ccache"
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G
+ccache -M 50G
 ccache -o compression=true
 ccache -z
