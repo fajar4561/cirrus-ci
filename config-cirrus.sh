9d34106cf917846f42bc3c321eb81d8615855dd9
WalkingDead <lexaxx14@gmail.com>
2021-11-11 15:45:59 +0300

    Update config-cirrus.sh

--- config-cirrus.sh
+++ config-cirrus.sh
@@ -11,8 +11,10 @@ echo "Repo Init/Sync"
 mkdir -p /tmp/rom
 cd /tmp/rom
 repo init -q --no-repo-verify --depth=1 -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b lineage-17.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/WalkingDead3/device_xiaomi_citrus -b lerish device/xiaomi/citrus
+git clone https://github.com/WalkingDead3/vendor_xiaomi_citrus -b los17 vendor/xiaomi/citrus
+
 
 # ccache settings
 echo "ccache"
