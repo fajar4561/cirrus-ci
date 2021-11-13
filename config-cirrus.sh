2ba98ceec25714c4af2183227ed5f30cbdeb0190
WalkingDead <lexaxx14@gmail.com>
2021-11-13 13:38:32 +0300

    cirrus: rebirth project

--- config-cirrus.sh
+++ config-cirrus.sh
@@ -1,13 +1,16 @@
-# ccache download
-echo "ccache downloader"
 df -h
 free -h
 nproc
 cat /etc/os*
 env
-##
-# Repo
-echo "Repo Init/Sync"
+
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 50G
+ccache -o compression=true
+ccache -z
+
 mkdir -p /tmp/rom
 cd /tmp/rom
 repo init -q --no-repo-verify --depth=1 -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
@@ -15,12 +18,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 git clone https://github.com/WalkingDead3/device_xiaomi_citrus -b lerish device/xiaomi/citrus
 git clone https://github.com/WalkingDead3/vendor_xiaomi_citrus -b los17 vendor/xiaomi/citrus
 
-
-# ccache settings
-echo "ccache"
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
-ccache -M 50G
-ccache -o compression=true
-ccache -z
+cd /tmp/rom
+source build/envsetup.sh
+lunch lineage_citrus-userdebug
+make bacon
+cd /tmp
+rclone copy /tmp/rom/out/target/product/citrus/Lineage*.zip WalkingDead:juice -P
+time rclone copy ccache.tar.gz WalkingDead:juice -P
