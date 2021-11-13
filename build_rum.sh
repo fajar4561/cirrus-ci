f91382fbbd6cec5c5586f49977c52f3e8a2d0391
WalkingDead <lexaxx14@gmail.com>
2021-11-13 13:37:03 +0300

    cirrus: delete build_rum.sh [skip ci]

--- build_rum.sh
+++
@@ -1,7 +0,0 @@
-cd /tmp/rom
-source build/envsetup.sh
-lunch lineage_citrus-userdebug
-make bacon
-cd /tmp
-rclone copy /tmp/rom/out/target/product/citrus/Lineage*.zip WalkingDead:juice -P
-time rclone copy ccache.tar.gz WalkingDead:juice -P
