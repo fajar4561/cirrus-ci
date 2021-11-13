49bf02b857e58fb73f783700aef749fb04d885db
WalkingDead <lexaxx14@gmail.com>
2021-11-13 13:36:29 +0300

    cirrus: delete InfoTG.sh [skip ci]

--- InfoTG.sh
+++
@@ -1,1 +0,0 @@
-curl -s "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendmessage" -d "text=Привет, сборка Evolution X (non-OSS) для Redmi 9T / Poco M3 началась! https://cirrus-ci.com/build/$CIRRUS_BUILD_ID $show" -d "chat_id=@WalkingCI" -d "parse_mode=HTML"
