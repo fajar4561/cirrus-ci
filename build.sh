6e18255ec2c29dcc64522cee40d922f17afeb27a
WalkingDead <lexaxx14@gmail.com>
2021-11-16 08:37:32 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,15 +13,15 @@ ccache -z
 
 mkdir -p /tmp/rom
 cd /tmp/rom
-repo init -q --no-repo-verify --depth=1 -u git://github.com/LineageOS/android.git -b lineage-17.1 -g default,-device,-mips,-darwin,-notdefault
+
+repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q
+git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b RR .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-git clone https://github.com/WalkingDead3/device_xiaomi_citrus -b lerish device/xiaomi/citrus
-git clone https://github.com/WalkingDead3/vendor_xiaomi_citrus -b los17 vendor/xiaomi/citrus
 
 cd /tmp/rom
 source build/envsetup.sh
-lunch lineage_citrus-userdebug
+lunch rr_citrus-userdebug
 make bacon
 cd /tmp
-rclone copy /tmp/rom/out/target/product/citrus/Lineage*.zip WalkingDead:juice -P
+rclone copy /tmp/rom/out/target/product/citrus/RROS*.zip WalkingDead:juice -P
 time rclone copy ccache.tar.gz WalkingDead:juice -P
