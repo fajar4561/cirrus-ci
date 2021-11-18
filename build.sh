c800286f7a8e8ad7be0b50b0441a47d16a72b840
WalkingDead <lexaxx14@gmail.com>
2021-11-18 15:31:50 +0300

    [builder] [skip ci]

--- build.sh
+++ build.sh
@@ -11,17 +11,10 @@ ccache -M 50G
 ccache -o compression=true
 ccache -z
 
-mkdir -p /tmp/rom
-cd /tmp/rom
-
-repo init --depth=1 --no-repo-verify -u https://github.com/ResurrectionRemix/platform_manifest.git -b Q
-git clone https://github.com/WalkingDead3/manifest.git --depth 1 -b RR .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-
 cd /tmp/rom
 source build/envsetup.sh
-lunch rr_citrus-userdebug
+lunch lineage_juice-userdebug
 make bacon
 cd /tmp
-rclone copy /tmp/rom/out/target/product/citrus/RROS*.zip WalkingDead:juice -P
+rclone copy /tmp/rom/out/target/product/citrus/Lineage*.zip WalkingDead:juice -P
 time rclone copy ccache.tar.gz WalkingDead:juice -P
