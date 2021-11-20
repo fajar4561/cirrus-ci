3362db776aefe9c64064fac61c55ec0e7d8a38af
WalkingDead <lexaxx14@gmail.com>
2021-11-20 09:58:18 +0300

    [cirrus-ci] - adapt to hentaiOS

--- build.sh
+++ build.sh
@@ -4,11 +4,6 @@ nproc
 cat /etc/os*
 env
 
-cd /tmp
-time aria2c $url -x16 -s50
-time tar xf ccache.tar.gz
-rm -rf ccache.tar.gz
-
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
@@ -18,8 +13,8 @@ ccache -z
 
 cd /tmp/rom
 source build/envsetup.sh
-lunch lineage_juice-userdebug
-make bacon -j$(nproc --all)
+lunch hentai_juice-userdebug
+make otapackage -j$(nproc --all)
 cd /tmp
-rclone copy /tmp/rom/out/target/product/juice/Lineage*.zip WalkingDead:juice -P
+rclone copy /tmp/rom/out/target/product/juice/hentai*.zip WalkingDead:juice -P
 time rclone copy ccache.tar.gz WalkingDead:juice -P
