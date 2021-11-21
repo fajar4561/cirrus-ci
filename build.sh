072d7d80190d52b6210c3a44d4f5b9e087276e87
WalkingDead <lexaxx14@gmail.com>
2021-11-21 08:40:17 +0400

    [skip ci]

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@ env
 cd /tmp/rom
 source build/envsetup.sh
 lunch hentai_juice-userdebug
-export SELINUX_IGNORE_NEVERALLOWS := true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 make otapackage -j$(nproc --all)
 cd /tmp
 rclone copy /tmp/rom/out/target/product/juice/hentai*.zip WalkingDead:juice -P
