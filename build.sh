baae0d38f937d59487979b575447e8df7fd7867c
WalkingDead <lexaxx14@gmail.com>
2021-11-25 09:38:22 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,12 +8,11 @@ export CCACHE_EXEC=$(which ccache)
 ccache -M 8G
 ccache -z
 
-cd /tmp/WeebProjekt
+cd /tmp/hentaiOS
 source build/envsetup.sh
-lunch weeb_juice-userdebug
-make weeb-prod
+lunch hentai_juice-userdebug
+make otapackage
 cd /tmp
-
 com () 
 { 
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
@@ -21,5 +20,5 @@ com ()
 
 time com ccache 1
 
-rclone copy /tmp/WeebProjekt/out/target/product/juice/Weeb*.zip WalkingDead:juice -P
+rclone copy /tmp/hentaiOS/out/target/product/juice/Weeb*.zip WalkingDead:juice -P
 rclone copy ccache.tar.gz WalkingDead:juice -P
