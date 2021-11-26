63c863f7b61a56a5740fa38f5843431a29ed783c
WalkingDead <lexaxx14@gmail.com>
2021-11-26 06:51:23 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,13 @@ set -x
 
 df -h && free -h && nproc && cat /etc/os* && env
 
+cd /tmp/hentaiOS
+git clone --depth=1 $repo_device -b $device_branch $path_device
+git clone --depth=1 $repo_kernel -b $kernel_branch $path_kernel
+git clone --depth=1 $repo_vendor -b $vendor_branch $path_vendor
+git clone --depth=1 $repo_clang -b $clang_branch $path_clang
+rm -rf device/generic/opengl-transport
+
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 ccache -M 8G
