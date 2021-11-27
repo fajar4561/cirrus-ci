13859c2a6817e81317c952b9f7d36dc39f45f31a
WalkingDead <lexaxx14@gmail.com>
2021-11-27 08:29:00 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #Set time zone to Moscow
 sudo ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/WalkingDead3/unicorn_kernel_citrus kernel && cd kernel
+git clone --depth=1 https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b lime-q-oss kernel && cd kernel
 git clone https://github.com/WalkingDead3/AnyKernel3 AnyKernel3
 git clone --depth=1 https://github.com/sohamxda7/llvm-stable clang
 git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
@@ -11,7 +11,6 @@ IMAGE=$(pwd)/out/arch/arm64/boot/Image
 START=$(date +"%s")
 KERNEL_DIR=$(pwd)
 PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-VERSION="$(cat arch/arm64/configs/vendor/citrus-perf_defconfig | grep "CONFIG_LOCALVERSION\=" | sed -r 's/.*"(.+)".*/\1/' | sed 's/^.//')"
 export KBUILD_BUILD_HOST=WalkingCI
 export KBUILD_BUILD_USER="WalkingDead :3"
 
@@ -34,12 +33,12 @@ function compile() {
 function sticker {
     curl -s -X POST "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendSticker" \
         -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
-        -d "chat_id=@WalkingCI"
+        -d chat_id=@WalkingCI
 }
 
 function sendinfo {
     curl -s -X POST "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendMessage" \
-        -d "chat_id=@WalkingCI" \
+        -d chat_id=@WalkingCI \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="<b>• Unicorn-Citrus •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Poco M3</b> (citrus)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 12</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #AOSP-Alpha"
@@ -48,18 +47,16 @@ function sendinfo {
 function push() {
     cd AnyKernel3
     ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendDocument" \
-        -F "chat_id=@WalkingCI" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Poco M3 (citrus)</b> | <b>Proton Clang 12</b>"
+	curl -F document=@"${ZIP}" -F 
+	"caption=Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s).
+    Apocalypse-Kernel for <b>Poco M3 (citrus)</b>" 
+	"https://api.telegram.org/bot2077617614:AAFOXH87MefDeZQO2LmTLVFAHYyQB8pW05Q/sendDocument?chat_id=@WalkingCI&parse_mode=Markdown"
 }
-
 
 # Zipping
 function zipping() {
     cd AnyKernel3 || exit 1
-    zip -r9 $VERSION-juice-$(date +%Y%m%d-%H%M).zip *
+    zip -r9 Apocalypse-juice-$(date +%Y%m%d-%H%M).zip *
     cd ..
 }
 
